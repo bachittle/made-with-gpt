ee4ceeef5915066115de663d2ca953c5094b82f7
Bailey Chittle <bachittle@gmail.com>
2023-04-02 21:55:17 -0400

    generate css based on creation and index docs

--- arcade-js2/create.sh
+++ arcade-js2/create.sh
@@ -4,4 +4,8 @@
 # initial folder structure
 # filegpt -f docs/create.md touch.sh -m gpt-4 >> touch.sh
 
-filegpt -f docs/create.md docs/explanation.md -m gpt-4 >> docs/explanation.md
+# generate explanations
+# filegpt -f docs/create.md docs/explanation.md -m gpt-4 >> docs/explanation.md
+
+# generate a main.css
+filegpt -f docs/create.md index.html css/main.css -m gpt-4 >> css/main.css
