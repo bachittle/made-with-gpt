295521db24b8977c8318f4a70952e6f04b62b05f
Bailey Chittle <bachittle@gmail.com>
2023-04-03 03:17:39 -0400

    stub frontend, fix issues as they arise

--- arcade-js2/create.sh
+++ arcade-js2/create.sh
@@ -8,4 +8,11 @@
 # filegpt -f docs/create.md docs/explanation.md -m gpt-4 >> docs/explanation.md
 
 # generate a main.css
-filegpt -f docs/create.md index.html css/main.css -m gpt-4 >> css/main.css
+# filegpt -f docs/create.md index.html css/main.css -m gpt-4 >> css/main.css
+
+# implement the game menu for breakout
+# filegpt -f docs/*.md index.html js/main.js >> js/main.js
+
+# fix div issue
+# filegpt -f index.html css/main.css js/main.js docs/issues/001.md -m gpt-4 >> docs/issues/001.md
+filegpt -f index.html css/main.css js/main.js docs/issues/002.md -m gpt-4 >> docs/issues/002.md
--- arcade-js2/js/main.js
+++ arcade-js2/js/main.js
@@ -1,1 +1,38 @@
-// main.js
+// implement main.js
+// start implementing step 2
+
+// Step 2: Implement basic game selection.
+
+// Define the list of available games.
+const gamesConfig = [
+    {
+        name: "Breakout",
+        file: "games/breakout.js"
+    }
+];
+
+// The generateMenu function generates menu items for each game in the list.
+function generateButtons() {
+    const buttonsContainer = document.getElementById("gameButtons");
+
+    gamesConfig.forEach((game) => {
+        const gameButton = document.createElement("button");
+        gameButton.innerHTML = game.name;
+        gameButton.classList.add("button");
+        gameButton.addEventListener("click", () => gameMenuItemClicked(game.file));
+        buttonsContainer.appendChild(gameButton);
+    });
+}
+
+// The gameMenuItemClicked function is called when a game menu item is clicked.
+function gameMenuItemClicked(gameFilePath) {
+    loadGame(gameFilePath);
+}
+
+// The loadGame function dynamically loads the selected game file and starts the game.
+function loadGame(gameFilePath) {
+    // TODO: Implement this function.
+}
+
+// Call the generateMenu function to populate the menu with games.
+generateButtons();
